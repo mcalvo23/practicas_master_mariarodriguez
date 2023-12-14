4d9aef7def04c84e3fa1d2d87ae543ab37bfc7e4
mcalvo23 <151920147+mcalvo23@users.noreply.github.com>
2023-12-14 11:46:30 +0100

    Add files via upload

---
+++ p26B_theyarecoming/js/fun.js
@@ -0,0 +1,14 @@
+const menuBtn = document.querySelector ('.webHeader-responsiveMenuButton');
+
+const menuResponsive = document.querySelector ('.responsiveMenu');
+
+const closeBtn = document.querySelector('.responsiveMenu-close');
+
+
+menuBtn.addEventListener('click', function(){
+    menuResponsive.classList.add('is-active');
+})
+
+closeBtn.addEventListener('click', function(){
+    menuResponsive.classList.remove('is-active');
+})
